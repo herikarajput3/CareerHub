781f054ab1c33ba4fc4054da34e8b38c4b984144
Herika <herikarajput3@gmail.com>
2025-06-25 13:05:37 +0530

    Installation of dependencies completed successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
+
 
 function App() {
 
   return (
     <>
-      CareerHub
+      <h1 className="text-3xl font-bold underline">CareerHub</h1>
+      <Button>login</Button>
     </>
   )
 }
