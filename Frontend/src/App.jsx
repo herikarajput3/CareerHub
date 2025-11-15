a23ef38c018da712fa4e1d1ce2419a522862e5d9
Herika <herikarajput3@gmail.com>
2025-11-15 12:02:03 +0530

    Initial commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/auth/Login'
