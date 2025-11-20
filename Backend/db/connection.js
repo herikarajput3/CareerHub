308d44972d30602b51ce6e729b6dd3f9ef10c712
Herika <herikarajput3@gmail.com>
2025-11-20 13:29:45 +0530

    initial commit

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -9,3 +9,20 @@ const connectDB = async () => {
     }
 }
 export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     console.log('MONGO_URI (debug) =', process.env.MONGO_URI);
+//     if (!process.env.MONGO_URI) {
+//       throw new Error('MONGO_URI is not defined. Check .env loading and path.');
+//     }
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log('mongodb connected successfully');
+//   } catch (error) {
+//     console.error('Failed to connect', error);
+//   }
+// }
+
+// export default connectDB;
--- Backend/index.js
+++ Backend/index.js
@@ -10,13 +10,13 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js"
 import path from "path";
 
-dotenv.config();
+dotenv.config({});
 // connect db
-connectDB();
+// connectDB();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-const _dirname = path.resolve();
+// const _dirname = path.resolve();
 
 
 // middleware
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://jobportal-youtube.onrender.com",
+    origin:"http://localhost:5173",
     credentials:true
 }
 app.use(cors(corsOptions));
@@ -36,11 +36,12 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get('*', (_,res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-});
+// app.use(express.static(path.join(_dirname, "/frontend/dist")));
+// app.get('*', (_,res) => {
+//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`server running at port ${PORT}`);
 });
